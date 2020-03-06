d55993311c9b140532a1938938100a01fb3fccd1
Moritz Herzog <mhmoritz3@gmail.com>
2020-03-06 20:30:49 +0100

    Working on first version

--- conanfile.py
+++ conanfile.py
@@ -1,65 +1,80 @@
-from conans import ConanFile, CMake, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
+from conans import ConanFile, CMake, tools
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports_sources = ["CMakeLists.txt"]
+class ProtobufConan(ConanFile):
+    name = "protobuf"
+    version = "3.5.2"
+    url = "https://github.com/bincrafters/conan-protobuf"
+    homepage = "https://github.com/protocolbuffers/protobuf"
+    topics = ("conan", "protobuf", "protocol-buffers", "protocol-compiler", "serialization", "rpc")
+    author = "Bincrafters <bincrafters@gmail.com>"
+    description = "Protocol Buffers - Google's data interchange format"
+    license = "BSD-3-Clause"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt", "protobuf.patch"]
     generators = "cmake"
-
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-
+    short_paths = True
+    options = {"shared": [True, False],
+               "with_zlib": [True, False],
+               "fPIC": [True, False],
+               "lite": [True, False]}
+    default_options = {"with_zlib": False,
+                       "shared": False,
+                       "fPIC": True,
+                       "lite": False}
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
-    _cmake = None
 
-    requires = (
-        "zlib/1.2.11"
-    )
+    @property
+    def _is_clang_x86(self):
+        return self.settings.compiler == "clang" and self.settings.arch == "x86"
 
-    def config_options(self):
-        if self.settings.os == 'Windows':
+    def configure(self):
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def requirements(self):
+        if self.options.with_zlib:
+            self.requires("zlib/1.2.11@conan/stable")
+
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
-        if self._cmake:
-            return self._cmake 
-
-        self._cmake = CMake(self)
-        self._cmake.definitions["BUILD_TESTS"] = False  # example
-        self._cmake.configure(build_folder=self._build_subfolder)
-
-        return self._cmake
+        cmake = CMake(self, set_cmake_flags=True)
+        cmake.definitions["protobuf_BUILD_TESTS"] = False
+        cmake.definitions["protobuf_WITH_ZLIB"] = self.options.with_zlib
+        cmake.definitions["protobuf_BUILD_PROTOBUF_LITE"] = self.options.lite
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["protobuf_MSVC_STATIC_RUNTIME"] = "MT" in self.settings.compiler.runtime
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
 
     def build(self):
+        tools.patch(base_path=self._source_subfolder, patch_file="protobuf.patch")
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
+            if self._is_clang_x86 or "arm" in str(self.settings.arch):
+                self.cpp_info.libs.append("atomic")
+
+        if self.settings.os == "Windows":
+            if self.options.shared:
+                self.cpp_info.defines = ["PROTOBUF_USE_DLLS"]
